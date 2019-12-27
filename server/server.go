85625f3172a7d7600ec869f6ae6ff43401ede06d
guanyu <yue.guanyu@hotmail.com>
2019-12-27 19:35:35 +0800

    prepare for sync method

--- server/server.go
+++ server/server.go
@@ -1,9 +1,21 @@
 package server
 
+// InfoType InfoType
+type InfoType int32
+
+const (
+	// InstanceState scene state
+	InstanceState InfoType = 0
+	// InstanceLevel InstanceLevel
+	InstanceLevel InfoType = 1
+	// InstanceEntry InstanceEntry
+	InstanceEntry InfoType = 2
+)
+
 // Server Server
 type Server struct {
 	SceneName          string
-	streamID           int
+	streamID           uint32
 	SceneInstanceChans map[string]chan bool
-	SceneInstanceInfos map[string]map[string]string
+	SceneInstanceInfos map[string]map[InfoType]string
 }
